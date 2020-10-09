0df5db255cb7a7071725a5a471e2318915026b90
Anton_Web_Crative <43933151+AntonWebCreative@users.noreply.github.com>
2020-10-09 12:56:11 +0400

    test repository
    test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.about__comment-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    });
+
+});
